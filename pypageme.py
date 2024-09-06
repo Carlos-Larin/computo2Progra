709e97b5291b4c79ebab4385efae1280cfd35b68
Carlos-Larin <nekokar701@gmail.com>
2024-09-06 17:13:02 -0600

    el inicio del juego

--- pypageme.py
+++ pypageme.py
@@ -1,1 +1,14 @@
 import pygame
+import pygame.display
+pygame.init()
+    #ancho,alto
+size=(500,800)
+
+#aqui para ir creando la vista de la ventana
+screen=pygame.display.set_mode(size)
+
+#los juegos se mantienen vivos en constante por los whiles(bucles infinitos)
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            SystemExit(exit())
