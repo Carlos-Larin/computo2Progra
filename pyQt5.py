d5c45cd8080c5e43400ab09c242bd092af3c1b18
Carlos-Larin <nekokar701@gmail.com>
2024-09-10 11:48:14 -0600

    clase miercoles

--- pyQt5.py
+++ pyQt5.py
@@ -1,8 +1,33 @@
 import sys
-from PyQt5 import QtGui,QtCore
+#el sys para ver los datos del sistema
 
-class ejemplo_Gui(QtGui.Qwidget):
+from PyQt5.QtWidgets import (QApplication,QWidget,QMainWindow,QPushButton,
+                             QLabel,QVBoxLayout,QLineEdit)
+
+"""
+super().__init()
+es como herencia padre he hijo
+y sirve para llamar al constructor "super()" lo que hace es:
+traer todo lo del padre y reemplazarlo en la hija
+practicamente un molde,
+una exportacion de la propiedad
+"""
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class VentanaMain(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(350,100,100,100)
+        self.setWindowTitle("Mi primera ventana no de casa")
+        # eje x, eje y, ancho, alto
+        self.setGeometry(100, 200, 300, 300)
+        boton = QPushButton("Haz clic aquí")
+        self.setCentralWidget(boton)
 
+# Inicializa la aplicación
+app = QApplication(sys.argv)
+# Crea una instancia de VentanaMain
+ventana = VentanaMain()
+ventana.show()
+# Ejecuta la aplicación
+app.exec()
