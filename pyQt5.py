69befd385ea20d9e8f42fc77d0aa245aa88d1ce4
Carlos-Larin <nekokar701@gmail.com>
2024-09-10 08:15:44 -0600

    PyQT5

--- pyQt5.py
+++ pyQt5.py
@@ -1,3 +1,8 @@
 import sys
-import pyQt5
-from 
+from PyQt5 import QtGui,QtCore
+
+class ejemplo_Gui(QtGui.Qwidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(350,100,100,100)
+
